93dcbb25d623f875bff88ef883087b510321075e
Kevin <kevinandres19kn@gmail.com>
2023-10-17 11:36:28 -0600

    Creacion del proyecto

---
+++ Calculadora2/src/main/java/calculadora2/Calculadora2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculadora2;
+
+/**
+ *
+ * @author Doble_R
+ */
+public class Calculadora2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
